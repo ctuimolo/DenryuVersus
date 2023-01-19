b4143fc88b236f20c1f272170130fbe5e59f49ea
Chaiz <tuimoloau@gmail.com>
2023-01-19 02:41:28 -0800

    new physics

--- GameObjects/Players/Bullet.cs
+++ GameObjects/Players/Bullet.cs
@@ -1,29 +1,39 @@
 using Godot;
 
 using Backgrounds;
+using Enemies;
 
 namespace Players
 {
-	public partial class Bullet : Area2D
+	public partial class Bullet : CharacterBody2D
 	{
+		[Export]
+		Area2D Hitbox;
+
 		[Export]
 		public AnimationPlayer Animator;
 
 		[Export]
-		public float BulletSpeed = 6;
+		public float BulletSpeed = 600;
 		public bool Active = false;
 		public SpaceBackground Background;
 
 		public BulletManager Manager;
 
+		private bool _queueDeactivate;
+
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
 			Active = false;
+			_queueDeactivate = false;
+
+			Hitbox.AreaEntered += Area2DEntered;
 		}
 
 		public void Fire(Vector2 fireFrom)
 		{
+			_queueDeactivate = false;
 			Visible = true;
 			Active	= true;
 			Animator.Stop();
@@ -33,22 +43,53 @@ namespace Players
 
 		public void Deactivate()
         {
+			Velocity = Vector2.Zero;
 			Visible = false;
 			Active = false;
 			Animator.Stop();
         }
 
+		public void Area2DEntered(Area2D other)
+		{
+			if (other is GenericEnemy)
+            {
+				Collide();
+            }
+        }
+
+		public void Collide()
+        {
+			Velocity = Vector2.Zero;
+			Animator.Play("hit");
+			Active = false;
+			_queueDeactivate = true;
+		}
+
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _Process(double delta)
 		{
+			if(_queueDeactivate && !Animator.IsPlaying())
+            {
+				Deactivate();
+				return;
+            }
+
 			if (Active)
 			{
-				Position += new Vector2(0, -BulletSpeed);
-				if( ToGlobal(Position).y < ToGlobal(Manager.OwningPlayer.Background.Position).y - Manager.OwningPlayer.Background.Size.y/2)
+				//Position += new Vector2(0, -BulletSpeed);
+
+				if ( ToGlobal(Position).y < ToGlobal(Manager.OwningPlayer.Background.Position).y - Manager.OwningPlayer.Background.Size.y/2)
 				{
 					Deactivate();
 				}
+
+				Velocity = new Vector2(0, -BulletSpeed);
 			}
+		}
+
+        public override void _PhysicsProcess(double delta)
+        {
+			MoveAndSlide();
 		}
 	}
 }
